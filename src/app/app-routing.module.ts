7f687b7d05a1e4234b66fe11f949c45ba3e606ba
muckystack <muckystack@gmail.com>
2020-12-23 23:11:41 -0600

    FEAT: Modularizo el auth y muevo el 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
