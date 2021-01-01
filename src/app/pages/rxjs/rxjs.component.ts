f1164b070fedeb742e19d0d1c90715105af30ac9
muckystack <muckystack@gmail.com>
2021-01-01 16:07:12 -0600

    FEAT: Observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -8,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    const obs$ = new Observable(observer => {
+      let i = -1;
+      const interval = setInterval(() => {
+        
+        i++;
+        observer.next(i);
+
+        if(i == 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+        if(i == 2) {
+          clearInterval(interval);
+          observer.error('i llego al valor de 2');
+        }
+
+      }, 1000);
+    });
+
+    obs$.subscribe(
+      value => console.log('Subs: ', value),
+      error => console.warn(error),
+      () => console.info('Terminado')
+    );
+
+  }
 
   ngOnInit(): void {
   }
