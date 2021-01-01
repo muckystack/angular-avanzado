9bc66bffd98b462008bdfa240501e0c9c8242d2b
muckystack <muckystack@gmail.com>
2021-01-01 17:25:31 -0600

    FEAT: Operador map en Observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import { retry, take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -12,17 +12,29 @@ export class RxjsComponent implements OnInit {
 
   constructor() {
 
-    this.returnObservable().pipe(
-      retry(1)
-    ).subscribe(
-      value => console.log('Subs: ', value),
-      error => console.warn(error),
-      () => console.info('Terminado')
-    );
+    // this.returnObservable().pipe(
+    //   retry(1)
+    // ).subscribe(
+    //   value => console.log('Subs: ', value),
+    //   error => console.warn(error),
+    //   () => console.info('Terminado')
+    // );
+
+    this.returnInterval().subscribe(
+      console.log
+    )
 
   }
 
   ngOnInit(): void {
+  }
+
+  returnInterval(): Observable<number> {
+    return interval(1000)
+    .pipe(take(4),
+    map(valor => {
+      return valor + 1;
+    }));
   }
 
   returnObservable(): Observable<number> {
